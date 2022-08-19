131076396605987ea0b55707fc8af913c2dc26eb
faris771 <farisabufarha33@gmail.com>
2022-08-19 18:21:04 +0300

    CLA ADDED, but still needed to be fixed when A is large

--- main.v
+++ main.v
@@ -11,6 +11,7 @@
 
 
 
+
 /*
 
     Gate
@@ -224,36 +225,167 @@ module SYSTEM(d,cout,a,b,s,cin);
 
 endmodule
 
-module TST_SYSTEM;
+// module TST_SYSTEM;
     
-    reg cin;
-    reg [3:0] a,b;
-    reg [1:0] s;
-    wire [3:0] d;
-    wire cout;
+//     reg cin;
+//     reg [3:0] a,b;
+//     reg [1:0] s;
+//     wire [3:0] d;
+//     wire cout;
 
-    SYSTEM DUMMY(.d(d), .cout(cout), .a(a), .b(b), .s(s), .cin(cin));
+//     SYSTEM DUMMY(.d(d), .cout(cout), .a(a), .b(b), .s(s), .cin(cin));
 
 
-    initial begin
-        // mux = 20 ns , 4bit adder : 45 =  65 + 5 for safety
+//     initial begin
+//         // mux = 20 ns , 4bit adder : 45 =  65 + 5 for safety
 
-        $display("SYSTEM TST BENCHMARK");
-        {s,cin} = 3'b000;
-        a = 4'b0000;
-        b = 4'b1111;
-        $display("a : %b | b : %b ",a,b);
-        repeat(8) begin
+//         $display("SYSTEM TST BENCHMARK");
+//         {s,cin} = 3'b000;
+//         a = 4'b0000;
+//         b = 4'b1111;
+//         $display("a : %b | b : %b ",a,b);
+//         repeat(8) begin
             
-            #70 {s,cin} = {s,cin} +  3'b001;
+//             #70 {s,cin} = {s,cin} +  3'b001;
 
 
-        end
+//         end
         
 
+//     end
+//     always #69 $display("Time %0d select = %b%b OUT= %b CARRY = %b\n",$time,s,cin,d,cout); // 1 sec diff between  changing of Ta and Tb and Tcin and printing the value   
+//     always #565 $finish; // 70 * 8 + 5 for safety
+
+
+
+
+// endmodule
+
+
+// ===================== look ahead adder ============================
+
+module CLA_ADDER(S , Cout , A , B , Cin); // SOURCE : https://www.geeksforgeeks.org/carry-look-ahead-adder/
+
+    input [3:0]A;
+    input [3:0]B; 
+    input Cin;
+    
+    output Cout; 
+    output[3:0]S; // sum
+    
+    wire [3:1]C;// C[i + 1 ]  = G[i]  + P[i]& c[i] ,  c[0] = cin
+
+    wire [0:3]P; // Pi = Ai xor Bi
+    wire [0:3]G; // Gi  = Ai and Bi 
+
+    
+    /// Making Pi
+    xor #11 p0(P[0] , A[0] , B[0]);
+    xor #11 p1(P[1] , A[1] , B[1]);
+    xor #11 p2(P[2] , A[2] , B[2]);
+    xor #11 p3(P[3] , A[3] , B[3]);
+    
+    /// Making Gi 
+    and #7 g0(G[0] , A[0] , B[0]);
+    and #7 g1(G[1] , A[1] , B[1]);
+    and #7 g2(G[2] , A[2] , B[2]);
+    and #7 g3(G[3] , A[3] , B[3]);
+    
+    /// Making C1
+    wire tmp1;
+    and #7 c11(tmp1 , P[0] , Cin); 
+    or  #7 c12(C[1] , G[0] , tmp1); // C[1] = G[0] + P[0]& cin
+    
+    /// Making C2
+    wire tmp2; // tmp2 = P[1]& cin
+    wire tmp3;// tmp3 = P[1]& cin & G[1]
+    and #7 c21(tmp2 , P[1] , G[0]);
+    and #7 c22(tmp3 , P[1] , P[0] , Cin);
+    or  #7 c23(C[2] , tmp2 , tmp3 , G[1]);
+    
+    /// Making C3
+    wire tmp4; // tmp4 = P[2]& G[1]
+    wire tmp5;// tmp5 = P[2]& P[1] & G[0]
+    wire tmp6; // tmp6 = P[2]& P[1] & P[0] & cin
+
+    and #7  c31(tmp4 , P[2] , G[1]);
+    and #7  c32(tmp5 , P[2] , P[1] , G[0]);
+    and #7  c33(tmp6 , P[2] , P[1] , P[0] , Cin);
+    or  #7  c34(C[3] , tmp4 , tmp5 , tmp6 , G[2]);
+    
+    /// Making Cout (C4) 
+    wire tmp7;
+    wire tmp8;
+    wire tmp9;
+    wire tmp10;
+
+    and #7  c41(tmp7 , P[3] , G[2]);
+    and #7  c42(tmp8 , P[3] , P[2] , G[1]);
+    and #7  c43(tmp9 , P[3] , P[2] , P[1] , G[0]);
+    and #7  c44(tmp10 , P[3] , P[2] , P[1] , P[0] , Cin);
+    or  #7  c45(Cout , tmp7 , tmp8 , tmp9 , tmp10 , G[3]);
+    
+    /// Making Sums // SUM = Pi XOR Ci
+    
+    xor #11 s0(S[0] , P[0] , Cin);
+    xor #11 s1(S[1] , P[1] , C[1]);
+    xor #11 s2(S[2] , P[2] , C[2]);
+    xor #11 s3(S[3] , P[3] , C[3]);
+    
+endmodule 
+
+module TST_CLA;
+
+    reg [3:0]A;
+    reg [3:0]B; 
+    reg Cin;
+    
+    wire Cout; 
+    wire [3:0]S; // sum
+    
+    CLA_ADDER DUMMY(.S(S), .Cout(Cout), .A(A), .B(B), .Cin(Cin));
+
+    initial begin
+        $display("CLA TST BENCHMARK");
+        
+        A = 4'b0000;
+        B = 4'b0000;
+        Cin = 1'b0;
+        
+        
+            
+        repeat(2**4  - 1) begin
+            B = B + 1;
+            #34 A = A + 1'b1;
+        end
+
+        // #34 A = 4'b1111;
+
+
+        // // #34 A = 4'b0001;
+        // #34 B = 4'b0001;
+        // // #34 A = 4'b0010;
+        // #34 B = 4'b1000;
+        // #34 A = 4'b1110;
+        // #34 B = 4'b0010;
+        // #34 Cin = 1'b1;
+        // #34 B = 4'b1111;
+        // #34 A = 4'b1000;
+
+
     end
-    always #69 $display("Time %0d select = %b%b OUT= %b CARRY = %b\n",$time,s,cin,d,cout); // 1 sec diff between  changing of Ta and Tb and Tcin and printing the value   
-    always #565 $finish; // 70 * 8 + 5 for safety
+    always #33 $display("Time %0d A = %b B = %b Cin = %b Cout = %b S = %b\n",$time,A,B,Cin,Cout,S); // 1 sec diff between  changing  and printing the value
+
+    always #500 $finish;
+
+
+
+
+
+
+
+
+
 
 
 
